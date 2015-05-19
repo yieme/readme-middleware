c56cdf22ca927431b6f8a82f19af025ee2eb3978
yieme <yieme@cormea.com>
2015-05-19 11:15:25 -0600

    initial

--- index.js
+++ index.js
@@ -4,28 +4,30 @@
  *  @module     readme-middleware
  */
 'use strict';
+var fs       = require('fs')
+var hogan    = require("hogan.js")
+var index    = fs.readFileSync(__dirname     + '/index.mustache', 'utf8')
+var pkg      = fs.readFileSync(process.cwd() + '/package.json', 'utf8')
+var template = hogan.compile(index)
+pkg.readme   = fs.readFileSync(process.cwd() + '/README.md', 'utf8')
+var page     = template.render(pkg)
 
 function readmeMiddleware(req, res, next) {
-  console.log(req.url)
-  next()
+  if (req.path == '/') {
+    res.set('Content-Type', 'text/html')
+    res.status(200).send(page)
+  } else {
+    next()
+  }
 }
+
 
 
 function server(options) {
-  options     = options || { }
-  var express = require('express')
-  var app     = express()
-  var port    = options.port || 3000
-  var pkg     = require('./package.json')
-
-  app.use(readmeMiddleware)
-  app.get('/', function(req, res){
-    res.send('OK');
-  })
-
-  app.listen(port)
-  console.log(pkg.name + '@' + pkg.version, 'listening on', port)
+  return require('middle-server')(options)
 }
 
-module.exports = readmeMiddleware
+
+
+module.exports        = readmeMiddleware
 module.exports.server = server
