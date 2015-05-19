07d01d333e5a8479f0f45d28ad5cdf279fa9c42e
yieme <yieme@cormea.com>
2015-05-19 11:15:40 -0600

    initial

--- test/index.js
+++ test/index.js
@@ -6,13 +6,4 @@ describe('readme-middleware', function() {
     readmeMiddleware = require('..')
     done()
   })
-
-  var expected = ["hello", "world"]
-  var expectedString = JSON.stringify(expected)
-  it('should eaual ' + expectedString, function(done) {
-    var test = readmeMiddleware(expected)
-    var json = JSON.stringify(test)
-    expect(json).to.equal(expectedString)
-    done()
-  })
 })
