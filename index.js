3fd69a4be5203f90bb58c3c4406c4b15bd566b5e
yieme <yieme@cormea.com>
2015-05-21 17:45:29 -0600

    add caching

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ var pkg        = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
 var template   = hogan.compile(index)
 pkg.readme     = fs.readFileSync(readmePath, 'utf8')
 var page       = template.render(pkg)
+var cacheDur   = (process.env.NODE_ENV == 'production') ? 60 * 60 * 1000 : 15 * 1000 // 1 hour in production, 15 seconds dev
 
 function readmeMiddleware(req, res, next) {
   if (req.path == '/') {
     res.set('Content-Type', 'text/html')
+    res.set('Cache-Control', 'public, max-age=' + cacheDur)
     res.status(200).send(page)
   } else {
     next()
